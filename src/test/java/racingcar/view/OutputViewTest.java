eeacc8e7bbd194e4bdeedd3afb1e6c60dd08e0c7
woohyeon <woohyeon6885@gmail.com>
2025-10-23 12:34:08 +0900

    feat: 게임 시작 프롬프트 출력 기능 구현

---
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OutputViewTest {
+
+    OutputView outputView = new OutputView();
+
+    @Test
+    void printStartPrompt() {
+        outputView.printStartPrompt();
+    }
+}

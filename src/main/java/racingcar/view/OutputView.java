989b4b5b657e7a45ed36f658b2392343d66b8773
woohyeon <woohyeon6885@gmail.com>
2025-10-26 00:04:47 +0900

    refactor(OutputView): 메서드 분리 및 순서 정리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -10,6 +10,7 @@ public class OutputView {
     public OutputView() {
     }
 
+//프롬프트 출력------------------------------------------------------------------------------------------------
     public void printStartPrompt() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
@@ -22,33 +23,62 @@ public class OutputView {
         System.out.println("\n실행 결과");
     }
 
+    public void printRaceResultPrompt() {
+        System.out.print("최종 우승자 : ");
+    }
+
+
+//Model 데이터 출력---------------------------------------------------------------------------------------------
+
     public void printCarName(Car car) {
         System.out.print(car.getName() + " : ");
     }
 
     public void printCarMovement(Car car) {
         int distance = car.getMovedDistance();
-        for (int i = 0; i < distance; i++) {
-            System.out.print(POSITION_INDICATOR);
-        }
-        System.out.println();
-    }
-
-    public void printRaceResultPrompt() {
-        System.out.print("최종 우승자 : ");
+        printPositionIndicatorsWith(distance);
+        changeLine();
     }
 
     public void printRaceResult(List<Car> winners) {
-        if (winners.size() == 1) {
-            System.out.println(winners.get(0).getName());
-        } else {
-            for (int i = 0; i < winners.size(); i++) {
-                System.out.print(winners.get(i).getName());
-                if (i < winners.size() - 1) {
-                    System.out.print(", ");
-                }
-            }
-            System.out.println();
+        if (isSoleWinner(winners)) {
+            printWinnerName(winners.get(0));
+            return;
+        }
+
+        printWinners(winners);
+    }
+
+    private static void printPositionIndicatorsWith(int distance) {
+        for (int i = 0; i < distance; i++) {
+            System.out.print(POSITION_INDICATOR);
         }
     }
+
+    private static boolean isSoleWinner(List<Car> winners) {
+        return winners.size() == 1;
+    }
+
+    private static void printWinners(List<Car> winners) {
+        for (int i = 0; i < winners.size(); i++) {
+            printWinnerName(winners.get(i));
+            printComma(winners, i);
+        }
+        changeLine();
+    }
+
+    private static void printWinnerName(Car winner) {
+        System.out.print(winner.getName());
+    }
+
+    private static void printComma(List<Car> winners, int i) {
+        if (i < winners.size() - 1) {
+            System.out.print(", ");
+        }
+    }
+
+    private static void changeLine() {
+        System.out.println();
+    }
+
 }

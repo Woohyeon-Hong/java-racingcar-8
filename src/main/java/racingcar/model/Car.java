84cad7df6b15c40b65b55c59f1cf566b459f0b15
woohyeon <woohyeon6885@gmail.com>
2025-10-26 22:36:34 +0900

    refactor(Car): findWinners 로직 개선

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,37 +2,13 @@ package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Car {
 
     private final String name;
     private int movedDistance;
-
-    public static List<Car> createRaceCarList(List<String> carNameList) {
-        List<Car> raceCarList = new ArrayList<>();
-        for (String carName : carNameList) {
-            raceCarList.add(new Car(carName));
-        }
-        return raceCarList;
-    }
-    public static List<Car> findWinners(List<Car> carList) {
-
-        List<Car> winners = new ArrayList<>();
-        winners.add(carList.get(0));
-
-        for (Car car : carList) {
-            int maxDistance = winners.get(0).getMovedDistance();
-            if (car.getMovedDistance() > maxDistance) {
-                winners = new ArrayList<>();
-                winners.add(car);
-            } else if (car.getMovedDistance() == maxDistance && !winners.contains(car)) {
-                winners.add(car);
-            }
-        }
-
-        return winners;
-    }
 
     public Car(String name) {
         this.name = name;
@@ -47,6 +23,33 @@ public class Car {
         return movedDistance;
     }
 
+
+//static 메서드------------------------------------------------------------------------------
+    public static List<Car> createRaceCarList(List<String> carNameList) {
+        List<Car> raceCarList = new ArrayList<>();
+        carNameList
+                .forEach((carName) -> raceCarList.add(new Car(carName)));
+        return raceCarList;
+    }
+    public static List<Car> findWinners(List<Car> carList) {
+        List<Car> winners = new ArrayList<>();
+        int maxMovedDistance = getMaxMovedDistance(carList);
+
+        //maxMovedDistance와 같은 거리를 이동한 모든 우승자들을 winners에 추가
+        for (Car car : carList) {
+            if (car.getMovedDistance() < maxMovedDistance) break;
+            winners.add(car);
+        }
+        return winners;
+    }
+
+    private static int getMaxMovedDistance(List<Car> carList) {
+        //movedDistance를 기준으로 내림차순 정렬 & 가장 멀리 이동한 자동차의 movedDistance 반환
+        carList.sort(Comparator.comparing(Car::getMovedDistance).reversed());
+        return carList.get(0).getMovedDistance();
+    }
+
+    //------------------------------------------------------------------------------
     public void move() {
         int number = Randoms.pickNumberInRange(0, 9);
         if (number >= 4) {
